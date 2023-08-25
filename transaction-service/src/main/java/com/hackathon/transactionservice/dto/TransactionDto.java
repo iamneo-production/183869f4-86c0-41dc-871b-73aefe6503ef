a10790c5bfebfb277baaff038d560230473be51b
VethaShana <vvvttt479@gmail.com>
2023-08-25 10:40:01 +0000

    Add entity and dto

---
+++ transaction-service/src/main/java/com/hackathon/transactionservice/dto/TransactionDto.java
@@ -0,0 +1,32 @@
+package com.hackathon.transactionservice.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionDto {
+
+    private int id;
+
+    private int customer_id;
+
+    private String trans_type;
+
+    private LocalDateTime date;
+
+    private double amount ;
+
+    private String currency;
+
+    private LocalDateTime created_at;
+
+             
+
+}
---
+++ transaction-service/src/main/java/com/hackathon/transactionservice/model/Transaction.java
@@ -0,0 +1,42 @@
+package com.hackathon.transactionservice.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="transaction")
+public class Transaction {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private int customer_id;
+
+    private String trans_type;
+
+    private LocalDateTime date;
+
+    private double amount ;
+
+    private String currency;
+
+    private LocalDateTime created_at;
+
+             
+
+}
