d63b4d0cba33dd0330769d60d11c1fccf33812e8
peirisabhi <abhishekdananjaya1@yahoo.com>
2023-08-26 11:00:20 +0000

    behavior analysis project added

---
+++ behavior-analysis-service/src/main/java/com/peirisabhi/behavioranalysisservice/BehaviorAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peirisabhi.behavioranalysisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehaviorAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehaviorAnalysisServiceApplication.class, args);
+	}
+
+}
