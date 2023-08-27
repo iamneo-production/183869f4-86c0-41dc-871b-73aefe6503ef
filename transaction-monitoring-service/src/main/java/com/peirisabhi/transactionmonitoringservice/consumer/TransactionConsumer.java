7e3257f3a3e3aadb7d397d9b947616da61113bc8
peirisabhi <abhishekdananjaya1@yahoo.com>
2023-08-27 10:15:44 +0000

    notification producer added with monitoring part

--- transaction-monitoring-service/src/main/java/com/peirisabhi/transactionmonitoringservice/consumer/TransactionConsumer.java
+++ transaction-monitoring-service/src/main/java/com/peirisabhi/transactionmonitoringservice/consumer/TransactionConsumer.java
@@ -2,15 +2,20 @@ package com.peirisabhi.transactionmonitoringservice.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.peirisabhi.transactionmonitoringservice.dto.TransactionDto;
+import com.peirisabhi.transactionmonitoringservice.service.TransactionMonitoringService;
 
 @Component
 public class TransactionConsumer {
+
+    @Autowired
+    TransactionMonitoringService transactionMonitoringService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TransactionConsumer.class);
 
@@ -24,6 +29,9 @@ public class TransactionConsumer {
         try {
             TransactionDto transactionDto = mapper.readValue(message, TransactionDto.class);
             // System.out.println(transactionDto);
+
+            transactionMonitoringService.isSuspicious(transactionDto);
+
         } catch (JsonProcessingException e) {
             
             e.printStackTrace();
