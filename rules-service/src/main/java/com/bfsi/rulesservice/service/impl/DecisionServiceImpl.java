2e3078872d0ba472f6aaec60fc57625bf8272f7f
peirisabhi <abhishekdananjaya1@yahoo.com>
2023-08-27 17:41:42 +0000

    rule trigger db save working

--- rules-service/src/main/java/com/bfsi/rulesservice/service/impl/DecisionServiceImpl.java
+++ rules-service/src/main/java/com/bfsi/rulesservice/service/impl/DecisionServiceImpl.java
@@ -1,5 +1,7 @@
 package com.bfsi.rulesservice.service.impl;
 
+import java.util.Date;
+
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,13 @@ import org.springframework.stereotype.Service;
 
 import com.bfsi.rulesservice.dto.NotificationDto;
 import com.bfsi.rulesservice.dto.TransactionDto;
+import com.bfsi.rulesservice.entity.RuleTrigger;
 import com.bfsi.rulesservice.producer.NotificationProducer;
+import com.bfsi.rulesservice.repository.RuleTriggerRepository;
 import com.bfsi.rulesservice.service.DecisionService;
 
 @Service
-public class DecisionServiceImpl implements DecisionService{
+public class DecisionServiceImpl implements DecisionService {
 
 	@Autowired
 	KieContainer kieContainer;
@@ -19,15 +23,29 @@ public class DecisionServiceImpl implements DecisionService{
 	@Autowired
 	NotificationProducer notificationProducer;
 
+	@Autowired
+	RuleTriggerRepository ruleTriggerRepository;
+
 	@Override
 	public TransactionDto checkTransactionFraud(TransactionDto transactionDto) {
-		
-		KieSession kieSession = kieContainer.newKieSession();
-        kieSession.insert(transactionDto);
-        kieSession.fireAllRules();
-        kieSession.dispose();
 
-		if(transactionDto.isFraud()){
+		KieSession kieSession = kieContainer.newKieSession();
+		kieSession.insert(transactionDto);
+		kieSession.fireAllRules();
+		kieSession.dispose();
+
+		if (transactionDto.isFraud()) {
+
+			RuleTrigger ruleTrigger = RuleTrigger.builder()
+					.customer_id(transactionDto.getCustomer_id())
+					.amount(transactionDto.getAmount())
+					.date(new Date())
+					.trans_type(transactionDto.getTrans_type())
+					.detectedRule("role-trigger-detection")
+					.build();
+
+					ruleTriggerRepository.save(ruleTrigger);
+
 			NotificationDto notificationDto = NotificationDto.builder()
 					.customer_id(transactionDto.getCustomer_id())
 					.amount(transactionDto.getAmount())
@@ -36,12 +54,12 @@ public class DecisionServiceImpl implements DecisionService{
 					.trans_type(transactionDto.getTrans_type())
 					.notificationType("role-trigger-detection")
 					.build();
-					
+
 			notificationProducer.sendMessage(notificationDto);
 		}
 
-        return transactionDto;
-		
+		return transactionDto;
+
 	}
-    
+
 }
