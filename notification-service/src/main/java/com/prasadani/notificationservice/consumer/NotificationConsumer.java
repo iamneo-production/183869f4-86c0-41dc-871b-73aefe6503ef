df04faac92770f946f04968b9f27d34c40c7530a
ruwaniprasadini <prasadiniruwani8@gmail.com>
2023-08-28 03:03:14 +0000

    data saving part is working

--- notification-service/src/main/java/com/prasadani/notificationservice/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/com/prasadani/notificationservice/consumer/NotificationConsumer.java
@@ -3,21 +3,27 @@ package com.prasadani.notificationservice.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prasadani.notificationservice.dto.NotificationDto;
+import com.prasadani.notificationservice.service.NotificationService;
+
 
 
 
 @Component
 public class NotificationConsumer {
+
+    @Autowired
+    NotificationService notificationService;
     
 private static final Logger LOGGER = LoggerFactory.getLogger(NotificationConsumer.class);
 
-    // @KafkaListener(topics = "${spring.kafka.topic-notification.name}", groupId = "${spring.kafka.consumer.group-id}")
+// @KafkaListener(topics = "${spring.kafka.topic-notification.name}", groupId = "${spring.kafka.consumer.group-id}")
     @KafkaListener(topics = "${spring.kafka.topic-notification.name}")
     public void consume(String message) {
         LOGGER.info(String.format("Message received -> %s", message));
@@ -25,8 +31,9 @@ private static final Logger LOGGER = LoggerFactory.getLogger(NotificationConsume
 
         ObjectMapper mapper = new ObjectMapper();
         try {
-           NotificationDto notificationDto = mapper.readValue(message, NotificationDto.class);
-            System.out.println(notificationDto);
+            NotificationDto notificationDto = mapper.readValue(message, NotificationDto.class);
+            //System.out.println(notificationDto);
+            notificationService.createNotification(notificationDto);
         } catch (JsonProcessingException e) {
             
             e.printStackTrace();
